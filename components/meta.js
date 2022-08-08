e0aa76551aa9b64b2957d4415e1e57bb86477bfa
Matthew Lewis <matthew.a.lewis7@gmail.com>
2022-08-08 13:12:47 -0400

    init

--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
+import Head from 'next/head';
+import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants';
 
 export default function Meta() {
   return (
@@ -32,11 +32,13 @@ export default function Meta() {
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      <meta name="description" content={`Annalisa Garofalo's blog`} />
       <meta
-        name="description"
-        content={`A statically generated blog example using Next.js and Sanity.io.`}
-      />
+        data-rh="true"
+        property="og:description"
+        content="Read writing from Annalisa Garofalo on Medium. sometimes poet, sometimes unidentified flying object. Every day, Annalisa Garofalo and thousands of other voices read, write, and share important stories on Medium."
+      ></meta>
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
-  )
+  );
 }

ae47bc03d6328403843f174f57d09a4e3fbe8961
Matthew Lewis <matthew.a.lewis7@gmail.com>
2022-08-08 14:28:25 -0400

    fixed footer.

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,17 @@
 import Container from './container';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Annalisas.site™
-          </h3>
+          <Link href="/">
+            <h3 className="text-4xl cursor-pointer hover:underline lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+              Annalisa Garofalo
+            </h3>
+          </Link>
+
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="mailto:annalisa.garofalo1@gmail.com"

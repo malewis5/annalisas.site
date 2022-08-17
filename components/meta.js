1de56fd83e7c90f8b34f710d98b78036666247d2
Matthew Lewis <matthew.a.lewis7@gmail.com>
2022-08-17 12:37:30 -0400

    Added sitemap.

--- components/meta.js
+++ components/meta.js
@@ -28,6 +28,7 @@ export default function Meta() {
         color="#000000"
       />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
+
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
---
+++ next-sitemap.config.mjs
@@ -0,0 +1,58 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: process.env.SITE_URL || 'https://annalisa.site',
+  generateIndexSitemap: false,
+  generateRobotsTxt: true, // (optional)
+  transform: async (config, path) => {
+    // custom function to ignore the path
+    // if (path == '/404') {
+    //   return null;
+    // }
+
+    // only create changefreq along with path
+    // returning partial properties will result in generation of XML field with only returned values.
+    if (path == '/') {
+      // This returns `path` & `changefreq`. Hence it will result in the generation of XML field with `path` and  `changefreq` properties only.
+      return {
+        loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+        changefreq: 'daily',
+        priority: 1,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        alternateRefs: config.alternateRefs ?? [],
+      };
+    }
+
+    // config for /posts/
+    if (/^.posts/gm.test(path)) {
+      return {
+        loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+        changefreq: 'weekly',
+        priority: 0.5,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        alternateRefs: config.alternateRefs ?? [],
+      };
+    }
+
+    // config for /me
+    if (/^.me/gm.test(path)) {
+      return {
+        loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+        changefreq: 'monthly',
+        priority: 0.7,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        alternateRefs: config.alternateRefs ?? [],
+      };
+    }
+
+    // Use default transformation for all other cases
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: 'weekly',
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
+};
+
+export default config;

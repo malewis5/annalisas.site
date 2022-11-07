0dc84083708ba0fd8c9d503761fd33c3740fb76f
Matthew Lewis <matthew.a.lewis7@gmail.com>
2022-11-06 23:03:24 -0500

    change hero post

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Head from 'next/head';
 import { CMS_NAME } from '../lib/constants';
 
 export default function Index({ allPosts, preview }) {
-  const heroPost = allPosts[0];
+  const heroPost = allPosts[2];
   const morePosts = allPosts.slice(1);
   return (
     <>
